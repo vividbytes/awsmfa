cf34090b8699ea177740786937374a4ead2e7b3f
Brian Ross <brian@bridev.com>
2020-02-27 16:10:31 -0800

    redo indentation

--- bin/awsmfa.js
+++ bin/awsmfa.js
@@ -13,21 +13,21 @@ let credentials;
 const credentialsFile = `${process.env.HOME}/.aws/credentials`;
 
 if (!token) {
-    console.error('Error: no token provided');
-    process.exit(2);
+  console.error('Error: no token provided');
+  process.exit(2);
 }
 
 if (!profile) {
-    console.error('Error: no profile provided');
-    process.exit(2);
+  console.error('Error: no profile provided');
+  process.exit(2);
 }
 
 try {
-    credentials = fs.readFileSync(credentialsFile, 'utf8');
+  credentials = fs.readFileSync(credentialsFile, 'utf8');
 } catch (e) {
-    console.error("Error: couldn't open credentials file");
-    console.error(e.message);
-    process.exit(3);
+  console.error("Error: couldn't open credentials file");
+  console.error(e.message);
+  process.exit(3);
 }
 
 const lines = credentials.split('\n');
@@ -37,31 +37,31 @@ const optionRegex = /(.*)=(.*)/;
 
 let currentProfile = '';
 const config = lines.reduce((acc, line) => {
-    const headerMatch = headerRegex.exec(line.trim());
-    if (headerMatch) {
-        currentProfile = headerMatch[1];
-        acc[currentProfile] = {};
-    } else {
-        const optionMatch = optionRegex.exec(line.trim());
-        if (optionMatch) {
-            const key = optionMatch[1].trim();
-            const value = optionMatch[2].trim();
-            acc[currentProfile][key] = value;
-        }
+  const headerMatch = headerRegex.exec(line.trim());
+  if (headerMatch) {
+    currentProfile = headerMatch[1];
+    acc[currentProfile] = {};
+  } else {
+    const optionMatch = optionRegex.exec(line.trim());
+    if (optionMatch) {
+      const key = optionMatch[1].trim();
+      const value = optionMatch[2].trim();
+      acc[currentProfile][key] = value;
     }
-    return acc;
+  }
+  return acc;
 }, {});
 
 if (!config[profile]) {
-    console.error(`Error: profile ${profile} not found in credentials file`);
-    process.exit(4);
+  console.error(`Error: profile ${profile} not found in credentials file`);
+  process.exit(4);
 }
 
 const mfaArn = config[profile].mfa_arn;
 
 if (!mfaArn) {
-    console.error(`Error: no mfa_arn defined for profile ${profile}`);
-    process.exit(5);
+  console.error(`Error: no mfa_arn defined for profile ${profile}`);
+  process.exit(5);
 }
 
 try {
@@ -74,22 +74,22 @@ try {
   );
   creds = JSON.parse(credString).Credentials;
 } catch (e) {
-    console.error(e.message);
-    process.exit(1);
+  console.error(e.message);
+  process.exit(1);
 }
 
 config.default = {
-    aws_access_key_id: creds.AccessKeyId,
-    aws_secret_access_key: creds.SecretAccessKey,
-    aws_session_token: creds.SessionToken
+  aws_access_key_id: creds.AccessKeyId,
+  aws_secret_access_key: creds.SecretAccessKey,
+  aws_session_token: creds.SessionToken
 };
 
 configString = Object.keys(config).reduce((acc, profile) => {
-    acc += `
+  acc += `
 
 [${profile}]`;
     Object.keys(config[profile]).forEach(key => {
-        acc += `
+      acc += `
 ${key} = ${config[profile][key]}`;
     });
     return acc;
