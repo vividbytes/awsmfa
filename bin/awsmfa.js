5def14a6d2562f5899ce132e154758d724c19397
Brian Ross <brian@bridev.com>
2020-06-09 07:23:32 -0700

    cleanup

--- bin/awsmfa.js
+++ bin/awsmfa.js
@@ -1,67 +1,71 @@
 #!/usr/bin/env node
 
-const execSync = require('child_process').execSync;
-const fs = require('fs');
+const execSync = require('child_process').execSync
+const fs = require('fs')
 
-const profile = process.argv[2];
-const token = process.argv[3];
-const duration = 86400;
+const profile = process.argv[2]
+const token = process.argv[3]
+const duration = 86400
 
-let creds;
-let credentials;
+let creds
+let credentials
 
-const credentialsFile = `${process.env.HOME}/.aws/credentials`;
+const credentialsFile = `${process.env.HOME}/.aws/credentials`
+const cacheEnabled = process.env.AWSMFA_CACHE_ENABLED || false
+const timeoutFile = `${process.env.AWSMFA_TIMEOUT_FILE}`
 
 if (!token) {
-  console.error('Error: no token provided');
-  process.exit(2);
+  console.error('Error: no token provided')
+  process.exit(2)
 }
 
 if (!profile) {
-  console.error('Error: no profile provided');
-  process.exit(2);
+  console.error('Error: no profile provided')
+  process.exit(2)
 }
 
 try {
-  credentials = fs.readFileSync(credentialsFile, 'utf8');
+  credentials = fs.readFileSync(credentialsFile, 'utf8')
 } catch (e) {
-  console.error("Error: couldn't open credentials file");
-  console.error(e.message);
-  process.exit(3);
+  console.error("Error: couldn't open credentials file")
+  console.error(e.message)
+  process.exit(3)
 }
 
-const lines = credentials.split('\n');
+const lines = credentials.split('\n')
 
-const headerRegex = /\[(.*)\]/;
-const optionRegex = /(.*)=(.*)/;
+const headerRegex = /\[(.*)\]/
+const optionRegex = /(.*)=(.*)/
 
-let currentProfile = '';
+let currentProfile = ''
+let currentProfileTimeout
+
 const config = lines.reduce((acc, line) => {
-  const headerMatch = headerRegex.exec(line.trim());
+  const headerMatch = headerRegex.exec(line.trim())
   if (headerMatch) {
-    currentProfile = headerMatch[1];
-    acc[currentProfile] = {};
+    currentProfile = headerMatch[1]
+    acc[currentProfile] = {}
   } else {
-    const optionMatch = optionRegex.exec(line.trim());
+    const optionMatch = optionRegex.exec(line.trim())
     if (optionMatch) {
-      const key = optionMatch[1].trim();
-      const value = optionMatch[2].trim();
-      acc[currentProfile][key] = value;
+      const key = optionMatch[1].trim()
+      const value = optionMatch[2].trim()
+      acc[currentProfile][key] = value
     }
   }
-  return acc;
-}, {});
+  return acc
+}, {})
 
 if (!config[profile]) {
-  console.error(`Error: profile ${profile} not found in credentials file`);
-  process.exit(4);
+  console.error(`Error: profile ${profile} not found in credentials file`)
+  process.exit(4)
 }
 
 const mfaArn = config[profile].mfa_arn;
 
 if (!mfaArn) {
-  console.error(`Error: no mfa_arn defined for profile ${profile}`);
-  process.exit(5);
+  console.error(`Error: no mfa_arn defined for profile ${profile}`)
+  process.exit(5)
 }
 
 try {
@@ -71,28 +75,37 @@ try {
     --token-code ${token} \
     --duration-seconds ${duration} \
     --output json`
-  );
-  creds = JSON.parse(credString).Credentials;
+  )
+
+  creds = JSON.parse(credString).Credentials
+  const now = new Date().getTime() / 1000
+  currentProfileTimeout = new Date(creds.Expiration).getTime() / 1000
+
 } catch (e) {
-  console.error(e.message);
-  process.exit(1);
+  console.error(e.message)
+  process.exit(1)
 }
 
 config.default = {
   aws_access_key_id: creds.AccessKeyId,
   aws_secret_access_key: creds.SecretAccessKey,
   aws_session_token: creds.SessionToken
-};
+}
 
 configString = Object.keys(config).reduce((acc, profile) => {
   acc += `
 
-[${profile}]`;
+[${profile}]`
     Object.keys(config[profile]).forEach(key => {
       acc += `
-${key} = ${config[profile][key]}`;
-    });
-    return acc;
-}, '');
+${key} = ${config[profile][key]}`
+    })
+    return acc
+}, '')
+fs.writeFileSync(credentialsFile, configString)
 
-fs.writeFileSync(credentialsFile, configString);
+if (cacheEnabled) {
+  let exportString = `export AWSMFA_EXPIRE_EPOCH=${currentProfileTimeout}\n`
+  exportString += `export AWSMFA_PROFILE=${profile}\n`
+  fs.writeFileSync(timeoutFile, exportString)
+}
